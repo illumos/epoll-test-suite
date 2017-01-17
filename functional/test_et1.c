e34dc4d40a8a23523d61fdf9da7ce23070f5ea52
Patrick Mooney <pmooney@pfmooney.com>
2017-01-17 20:52:43 +0000

    Add tests for nesting and POLLET

---
+++ functional/test_et1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/eventfd.h>
+#include <sys/epoll.h>
+
+#include "common.h"
+
+int evfd = -1;
+int ready = 0;
+pthread_mutex_t lock;
+
+void *
+writer_func(void *args) {
+	uint64_t val = 1;
+
+	ready = 1;
+	pthread_mutex_lock(&lock);
+	pthread_mutex_unlock(&lock);
+
+	/* allow the main thread time to enter the epoll_ctl */
+	usleep(1000);
+
+	write(evfd, &val, sizeof (val));
+	return (NULL);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t thread_write;
+	struct epoll_event ev;
+	int epfd;
+
+	test_init(argc, argv);
+
+	if ((evfd = eventfd(0, EFD_CLOEXEC|EFD_NONBLOCK)) < 0) {
+		test_fatal("eventfd()");
+	}
+	if ((epfd = epoll_create1(0)) < 0) {
+		test_fatal("epoll_create1()");
+	}
+
+	ev.events = EPOLLIN|EPOLLOUT|EPOLLET;
+	ev.data.fd = evfd;
+	if (epoll_ctl(epfd, EPOLL_CTL_ADD, evfd, &ev) < 0) {
+		test_fatal("epoll_ctl(EPOLL_CTL_ADD)");
+	}
+
+	/* The eventfd is writable */
+	test_equal(epoll_wait(epfd, &ev, 1, 0), 1);
+	test_equal(ev.events, EPOLLOUT);
+	test_equal(ev.data.fd, evfd);
+
+	/* ET should yield no event now */
+	test_equal(epoll_wait(epfd, &ev, 1, 0), 0);
+
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		test_fatal("pthread_mutex_init");
+	}
+	pthread_mutex_lock(&lock);
+	pthread_create(&thread_write, NULL, writer_func, NULL);
+
+	while (!ready) {
+		usleep(1000);
+	}
+	pthread_mutex_unlock(&lock);
+
+	/* The eventfd should report as readable once the above write succeeds */
+	test_equal(epoll_wait(epfd, &ev, 1, 1000), 1);
+	test_equal(ev.events, EPOLLIN|EPOLLOUT);
+	test_equal(ev.data.fd, evfd);
+
+	pthread_join(thread_write, NULL);
+	test_done();
+}
+
---
+++ functional/test_nested1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+
+#include "common.h"
+
+int
+main(int argc, char **argv)
+{
+	int efd, nefd, pipefd[2];
+	struct epoll_event ev;
+
+	test_init(argc, argv);
+
+	if (pipe(pipefd) != 0) {
+		test_fatal("pipe()");
+	}
+
+	if ((efd = epoll_create1(0)) < 0) {
+		test_fatal("epoll_create1()");
+	}
+	ev.events = EPOLLIN;
+	ev.data.fd = pipefd[0];
+	if (epoll_ctl(efd, EPOLL_CTL_ADD, pipefd[0], &ev) < 0) {
+		test_fatal("epoll_ctl(EPOLL_CTL_ADD)");
+	}
+
+	if ((nefd = epoll_create1(0)) < 0) {
+		test_fatal("epoll_create1()");
+	}
+	/* cast a wide net */
+	ev.events = EPOLLIN|EPOLLRDNORM|EPOLLRDBAND|EPOLLMSG;
+	ev.events |= EPOLLOUT|EPOLLWRNORM|EPOLLWRBAND;
+	ev.data.fd = efd;
+	if (epoll_ctl(nefd, EPOLL_CTL_ADD, efd, &ev) < 0) {
+		test_fatal("epoll_ctl(EPOLL_CTL_ADD)");
+	}
+
+	/* No events should be pending yet */
+	test_equal(epoll_wait(efd, &ev, 1, 0), 0);
+	test_equal(epoll_wait(nefd, &ev, 1, 0), 0);
+
+	if (write(pipefd[1], "\0", 1) != 1) {
+		test_fatal("write()");
+	}
+
+	test_equal(epoll_wait(efd, &ev, 1, 0), 1);
+	test_equal(ev.events, EPOLLIN);
+	test_equal(ev.data.fd, pipefd[0]);
+
+	test_equal(epoll_wait(nefd, &ev, 1, 0), 1);
+	test_equal(ev.events, EPOLLIN|EPOLLRDNORM);
+	test_equal(ev.data.fd, efd);
+
+	test_done();
+}
