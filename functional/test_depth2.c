b463fb5431f1dc979126d23515b3de8fc44054bd
Patrick Mooney <pmooney@pfmooney.com>
2017-02-08 16:32:29 -0600

    Add more detail to test_depth2

--- functional/test_depth2.c
+++ functional/test_depth2.c
@@ -12,7 +12,7 @@ int
 main(int argc, char **argv)
 {
 	int pipe_fd[2], test_fd[T_TARGET_DEPTH + 1];
-	int efd, res, i;
+	int efd, pefd, res, i;
 	struct epoll_event ev;
 	char buf;
 
@@ -41,6 +41,8 @@ main(int argc, char **argv)
 			test_fatal("epoll_ctl(EPOLL_CTL_ADD)");
 		}
 		test_fd[i] = efd;
+		/* Keep track of the parent epoll fd, too */
+		pefd = test_fd[i-1];
 	}
 
 	ev.events = EPOLLIN;
@@ -48,7 +50,25 @@ main(int argc, char **argv)
 	if (epoll_ctl(efd, EPOLL_CTL_ADD, pipe_fd[0], &ev) < 0) {
 		test_equal(errno, EINVAL);
 	} else {
-		test_fail("too deep");
+		test_fatal("too deep");
+	}
+
+	/*
+	 * Remove the last epoll descriptor from the chain, add the pipe to it,
+	 * then attempt to re-add it back.
+	 */
+	if (epoll_ctl(pefd, EPOLL_CTL_DEL, efd, NULL) < 0) {
+			test_fatal("epoll_ctl(EPOLL_CTL_DEL)");
+	}
+	if (epoll_ctl(efd, EPOLL_CTL_ADD, pipe_fd[0], &ev) < 0) {
+			test_fatal("epoll_ctl(EPOLL_CTL_ADD)");
+	}
+
+	ev.data.fd = efd;
+	if (epoll_ctl(pefd, EPOLL_CTL_ADD, efd, &ev) < 0) {
+		test_equal(errno, EINVAL);
+	} else {
+		test_fatal("too deep");
 	}
 
 	test_done();
