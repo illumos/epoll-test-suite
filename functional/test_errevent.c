143d9d514cb473ff02ad6b70a493d89337a9c490
Patrick Mooney <pmooney@pfmooney.com>
2019-01-02 20:06:29 +0000

    Address POLLHUP behavior on illumos pipes

--- functional/test_errevent.c
+++ functional/test_errevent.c
@@ -6,6 +6,20 @@
 
 #include "common.h"
 
+#define	TARGETEVENT	EPOLLERR
+#if defined(_OS_SUNOS)
+/*
+ * Unfortunately, the behavior of pipe closure differs between Linux and
+ * illumos.  Linux will emit EPOLLERR when the pipe writer (fd[1]) is closed
+ * and EPOLLHUP when the reader is closed.  On illumos, EPOLLHUP is emitted
+ * when either side is closed on the other.
+ *
+ * Redefining this on SunOS makes this test a little redundant with
+ * test_hupevent, but it ensures proper coverage on Linux.
+ */
+#undef	TARGETEVENT
+#define	TARGETEVENT	EPOLLHUP
+#endif
 
 int
 main(int argc, char **argv)
@@ -41,7 +55,7 @@ main(int argc, char **argv)
 	 * when it has not been set in the events for the subscription.
 	 */
 	test_equal(epoll_wait(efd, &ev, 1, 0), 1);
-	test_equal(ev.events, EPOLLERR);
+	test_equal(ev.events, TARGETEVENT);
 
 	if (epoll_ctl(efd, EPOLL_CTL_DEL, pipefd[1], NULL) < 0) {
 		test_fatal("epoll_ctl(EPOLL_CTL_DEL)");
@@ -55,7 +69,7 @@ main(int argc, char **argv)
 
 	/* Make sure it behaves as expected in oneshot mode too */
 	test_equal(epoll_wait(efd, &ev, 1, 0), 1);
-	test_equal(ev.events, EPOLLERR);
+	test_equal(ev.events, TARGETEVENT);
 	test_equal(epoll_wait(efd, &ev, 1, 0), 0);
 
 	test_done();
